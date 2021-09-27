320b875da549e63ea4cbb377802a3af70a1c548d
kuba05 <kopcil.jakub@gmail.com>
2021-09-27 07:54:21 +0200

    FEN fixed

--- components/helper.js
+++ components/helper.js
@@ -9,40 +9,54 @@
  **/
 const parsePGN = (pgn) => {
   return {
-    "FEN": "3/1x1/3",
+    "FEN": "5/5/2x2/5/5",
     "PlayerOnMove": 1
   }
 }
 /**
  * FEN: string
  * 
- * output: int[][]
+ * output: int[][] - board (null is empty square, 0 is an X, 1 is an O)
  * 
  * FIXME
  **/
 const parseFEN = (FEN) => {
-  /*
+  
   //for each row
-  FEN.split("/").map(
+  let board = FEN.split("/").map(
     row => {
       let output = [];
-      let number = "";
+      let numberOfEmptyFields = 0;
       for (let char of row) {
-        if (char in ["x", "O"]) {
-          output.push(1);
-        } else {
-          number += char;
+        let digit = parseInt(char);
+        
+        //char is not a digit
+        if (isNaN(digit)) {
+          for (let i = 0; i < numberOfEmptyFields; i++) {
+            output.push(null);
+          }
+          numberOfEmptyFields = 0;
+          if (char === "x") {
+            output.push(0);
+          } else if (char === "O") {
+            ouput.push(1);
+          }
+          continue;
         }
+        
+        //char is a digit
+        numberOfEmptyFields = numberOfEmptyFields * 10 + digit;
+      }
+      for (let i = 0; i < numberOfEmptyFields; i++) {
+        output.push(null);
       }
       return output;
     }
-  )*/
+  )
   
-  return [
-    [null, null, null],
-    [null, 0, null],
-    [null, null, null]
-  ]
+  console.log(board);
+  
+  return board;
 }
 
 export default {parseFEN, parsePGN};
